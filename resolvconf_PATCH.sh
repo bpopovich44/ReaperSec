ea623d07bad9dc08ee3af9c663bdb46c58f2d0fa
bpopovich44 <bpopovich4@gmail.com>
2018-02-12 11:17:36 -0700

    Third push to master repo

--- resolvconf_PATCH.sh
+++ resolvconf_PATCH.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-##########################
+###################################################################################################################################
 ##
 ## README: find what is creating /run/resolvconf/interface/NetworkManager and replicate 
-##
+## 
+## Installed update-resolve.config for vpn.  When reboot, the script doesnt recreate the below files and system cannot get an ip 
 ## When reboot, these files are deleted.  This script adds files and sym links needed for operation of vpn
 ##
-###########################
+###################################################################################################################################
 
 
 mkdir /run/resolvconf/
